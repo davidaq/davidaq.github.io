01cc5c926bc6750484033027392e616694306937
DavidAQ <aq@niven.cn>
2014-11-20 23:58:22 +0800

    ffmpeg

---
+++ static/js/codelite.js
@@ -0,0 +1,16 @@
+$(function() {
+    var primitives = ['int','long','float','double','short','byte','unsigned','char','bool','boolean','void'];
+    var reserved = ['if','while','do','for','until','function',
+        'public','private','protected','class','struct','static',
+        'const','final','try','catch','except','finally','in',
+        'switch','case','new','delete',''];
+    var specialPrefix = ['@','$','#'];
+    var lbrace = ['(','[','{'];
+    var rbrace = [')',']','}'];
+    var quotes = ['"',"'"];
+    function codeLite() {
+    }
+    $('.article .content pre code').each(function() {
+
+    });
+});
