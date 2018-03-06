31c88344b3109e03a6f732ca6ff05e1457df61c4
安秦 <anqin@ele.me>
2018-03-06 14:38:12 +0800

    gobang

---
+++ static/gobang-ai/game.js
@@ -0,0 +1,84 @@
+const TIE = 0;
+const EMPTY = 0;
+const BLACK = 1;
+const WHITE = 2;
+
+const BOARD_SIZE = 11;
+
+class Game {
+  constructor (players) {
+    this.players = players;
+    this.currentPlayer = BLACK;
+    this.prevPlay = [-1, -1];
+    this.remainRound = BOARD_SIZE * BOARD_SIZE;
+    this.board = [];
+    var line = [];
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      line.push(EMPTY);
+    }
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      this.board.push(line.slice(0));
+    }
+    this.getPlayerDecision();
+  }
+
+  getPlayerDecision () {
+    this.players[this.currentPlayer - 1].decide(this, (x, y) => {
+      this.remainRound--;
+      this.board[y][x] = this.currentPlayer;
+      this.prevPlay = [x, y];
+      if (!this.checkWin(x, y)) {
+        if (this.currentPlayer === BLACK) {
+          this.currentPlayer = WHITE;
+        } else {
+          this.currentPlayer = BLACK;
+        }
+        this.getPlayerDecision();
+      }
+    });
+  }
+
+  checkWin (x, y) {
+    const lines = [
+      [1, 0],
+      [0, 1],
+      [1, 1],
+      [1, -1],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [dx, dy] = lines[i];
+      let count = 1;
+      const check = (dir) => {
+        for (let j = 1; j < 5; j++) {
+          var cx = x - dx * j * dir;
+          var cy = y - dy * j * dir;
+          if (cx < 0 || cx >= BOARD_SIZE || cy < 0 || cy >= BOARD_SIZE) {
+            break;
+          } else if (this.board[cy][cx] === this.currentPlayer) {
+            count++;
+          } else {
+            break;
+          }
+        }
+      }
+      check(1);
+      check(-1);
+      if (count >= 5) {
+        this.players.forEach(player => {
+          player.end(this, this.currentPlayer);
+        });
+        this.onEnd && this.onEnd(this.currentPlayer);
+        return true;
+      }
+    }
+    if (this.remainRound === 0) {
+      this.players.forEach(player => {
+        player.end(this, TIE);
+      });
+      this.onEnd && this.onEnd(TIE);
+      return true;
+    }
+    return false
+  }
+}
+
---
+++ static/gobang-ai/player-ui.js
@@ -0,0 +1,89 @@
+const CELL_SIZE = 50;
+
+class PlayerUI {
+  constructor ($dom) {
+    this.$dom = $dom;
+  }
+
+  decide (game, callback) {
+    this.$dom.innerHTML = '';
+    Object.assign(this.$dom.style, {
+      width: `${CELL_SIZE * (BOARD_SIZE - 0.25)}px`,
+      height: `${CELL_SIZE * (BOARD_SIZE - 0.25)}px`,
+      background: '#9D9',
+    });
+    for (let y = 0; y < BOARD_SIZE; y++) {
+      const $row = document.createElement('div');
+      Object.assign($row.style, {
+        position: 'absolute',
+        boxSizing: 'border-box',
+        left: `${CELL_SIZE * 0.5}px`,
+        top: `${CELL_SIZE * (y + 0.5)}px`,
+        width: `${1 + CELL_SIZE * (BOARD_SIZE - 1)}px`,
+        height: '1px',
+        background: '#000',
+        zIndex: 0,
+      });
+      this.$dom.appendChild($row);
+      const $col = document.createElement('div');
+      Object.assign($col.style, {
+        position: 'absolute',
+        boxSizing: 'border-box',
+        left: `${CELL_SIZE * (y + 0.5)}px`,
+        top: `${CELL_SIZE * 0.5}px`,
+        width: '1px',
+        height: `${1 + CELL_SIZE * (BOARD_SIZE - 1)}px`,
+        background: '#000',
+        zIndex: 0,
+      });
+      this.$dom.appendChild($col);
+      const line = game.board[y];
+      for (let x = 0; x < BOARD_SIZE; x++) {
+        const positionState = line[x];
+        const $pos = document.createElement('div');
+        Object.assign($pos.style, {
+          position: 'absolute',
+          width: `${CELL_SIZE * 0.8}px`,
+          height: `${CELL_SIZE * 0.8}px`,
+          borderRadius: `${CELL_SIZE}px`,
+          top: `${CELL_SIZE * (0.1 + y)}px`,
+          left: `${CELL_SIZE * (0.1 + x)}px`,
+          zIndex: 1,
+        });
+        if (positionState === EMPTY) {
+          if (!callback) {
+            continue;
+          }
+          Object.assign($pos.style, {
+            width: `${CELL_SIZE * 0.4}px`,
+            height: `${CELL_SIZE * 0.4}px`,
+            top: `${CELL_SIZE * (0.3 + y)}px`,
+            left: `${CELL_SIZE * (0.3 + x)}px`,
+            cursor: 'pointer',
+            background: 'rgba(0, 100, 0, 0.0)',
+            transition: 'background 0.3s',
+          });
+          $pos.addEventListener('mouseover', () => {
+            $pos.style.background = 'rgba(0, 100, 0, 0.3)';
+          });
+          $pos.addEventListener('mouseout', () => {
+            $pos.style.background = 'rgba(0, 100, 0, 0.0)';
+          });
+          $pos.addEventListener('click', () => {
+            callback(x, y);
+          });
+        } else {
+          Object.assign($pos.style, {
+            boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.2)',
+            background: positionState === BLACK ? '#000' : '#FFF',
+          });
+        }
+        this.$dom.appendChild($pos);
+      }
+    }
+  }
+
+  end (game, winner) {
+    this.decide(game);
+  }
+}
