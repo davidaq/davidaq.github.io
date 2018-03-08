7a73f47689c2ca7c4a109ec15ed950f71cb47507
安秦 <anqin@ele.me>
2018-03-08 21:07:17 +0800

    dqn

--- static/gobang-ai/game.js
+++ static/gobang-ai/game.js
@@ -3,13 +3,12 @@ const EMPTY = 0;
 const BLACK = 1;
 const WHITE = 2;
 
-const BOARD_SIZE = 11;
+const BOARD_SIZE = 9;
 
 class Game {
   constructor (players, rated) {
     this.players = players;
     this.rated = rated;
-    this.score = [0, 0];
     this.currentPlayer = BLACK;
     this.prevPlay = null;
     this.remainRound = BOARD_SIZE * BOARD_SIZE;
@@ -48,8 +47,7 @@ class Game {
       [1, 1],
       [1, -1],
     ];
-    const elapsePenalty = Math.floor((this.elapseRound - 1) / 2);
-    this.score[this.currentPlayer - 1] -= elapsePenalty;
+    let reward = 0;
     for (let i = 0; i < lines.length; i++) {
       const [dx, dy] = lines[i];
       let count = 1;
@@ -72,64 +70,42 @@ class Game {
       }
       check(1);
       check(-1);
-      if (count >= 5) {
-        this.score[this.currentPlayer - 1] += 100;
+      if (count >= 4) {
+        this.isWin = true;
+        this.onReward && this.onReward(this.currentPlayer, 5000);
         this.players.forEach(player => {
           player.end(this, this.currentPlayer);
         });
-        this.onEnd && this.onEnd(this.currentPlayer, this.score);
+        this.onEnd && this.onEnd(this.currentPlayer);
         return true;
       }
       if (this.rated) {
-        let score = 0;
         // 检查活连
-        if (count === 4) {
+        if (count === 3) {
           if (open === 2) {
-            score += 120;
+            reward += 50;
           } else if (open === 1) {
-            score += 90
-          }
-        } else if (count === 3) {
-          if (open === 2) {
-            score += 50;
-          } else if (open === 1) {
-            score += 20;
+            reward += 30
           }
         } else if (count === 2) {
           if (open === 2) {
-            score += 10;
+            reward += 30;
+          } else if (open === 1) {
+            reward += 10;
           }
         }
-        // 检查堵住对方
-        count = 0;
-        const check = (dir) => {
-          for (let j = 1; j < 5; j++) {
-            var cx = x - dx * j * dir;
-            var cy = y - dy * j * dir;
-            if (cx < 0 || cx >= BOARD_SIZE || cy < 0 || cy >= BOARD_SIZE) {
-              break;
-            } else if (this.board[cy][cx] === EMPTY) {
-              break;
-            } else if (this.board[cy][cx] !== this.currentPlayer) {
-              count++;
-            } else {
-              break;
-            }
-          }
-        };
-        check(1);
-        check(-1);
-        if (count > 3) {
-          score += count * 30;
-        }
-        this.score[this.currentPlayer - 1] += score;
       }
     }
+    if (this.elapseRound < 30) {
+      reward *= 30 - this.elapseRound;
+    }
+    reward -= 1;
+    this.onReward && this.onReward(this.currentPlayer, reward);
     if (this.remainRound === 0) {
       this.players.forEach(player => {
         player.end(this, TIE);
       });
-      this.onEnd && this.onEnd(TIE, this.score);
+      this.onEnd && this.onEnd(TIE);
       return true;
     }
     return false
--- static/gobang-ai/player-ai.js
+++ static/gobang-ai/player-ai.js
@@ -1,101 +1,90 @@
 class PlayerAI {
-  constructor (coreJSON) {
-    coreJSON = null;
+  constructor (coreJSON, learn) {
+    this.learn = learn;
+    this.shouldLearn = false;
+    this.willReward = 0;
     this.initCore(coreJSON);
   }
 
   initCore (json) {
-    this.net = new convnetjs.Net();
+    const env = {
+      getNumStates () {
+        return BOARD_SIZE * BOARD_SIZE * 3;
+      },
+      getMaxNumActions () {
+        return BOARD_SIZE * BOARD_SIZE;
+      },
+    };
+    const spec = {
+      alpha: 0.01,
+      epsilon: this.learn ? 0.2 : 0,
+      gamma: 0.5,
+      num_hidden_units: BOARD_SIZE * 5,
+      experience_add_every: 1,
+      experience_size: 1000,
+    };
+    this.brain = new RL.DQNAgent(env, spec);
     if (json) {
-      this.net.fromJSON(json);
-    } else {
-      this.net.makeLayers([
-        { type: 'input', out_sx: BOARD_SIZE, out_sy: BOARD_SIZE, out_depth: 2 },
-        { type: 'conv', sx: 4, filters: 8, stride: 1, pad: 0, activation: 'relu' },
-        { type: 'pool', sx: 2, stride: 2 },
-        { type: 'regression', num_neurons: 1 }
-      ]);
+      this.brain.fromJSON(json);
     }
-    this.netInput = new convnetjs.Vol(BOARD_SIZE, BOARD_SIZE, 2);
+    this.netInput = new Array(env.getNumStates());
+  }
+
+  getCore () {
+    return this.brain.toJSON();
+  }
+
+  reward (value) {
+    this.willReward += value;
   }
 
   decide (game, callback) {
+    this.learnFromReward();
     const input = this.netInput;
     let i = 0;
-    const candidates = {};
     game.board.forEach((line, y) => line.forEach((v, x) => {
+      input[i++] = 0.0;
       if (v === EMPTY) {
-        input.set(x, y, 0, 0.0);
-        input.set(x, y, 0, 0.0);
+        input[i++] = 0.0;
+        input[i++] = 0.0;
       } else {
-        for (let dy = -2; dy <= 2; dy++) {
-          for (let dx = -2; dx <= 2; dx++) {
-            const cx = x + dx;
-            const cy = y + dy;
-            if (cx >= 0 && cx < BOARD_SIZE && cy >=0 && cy < BOARD_SIZE) {
-              if (game.board[cy][cx] === EMPTY) {
-                candidates[cx + cy * BOARD_SIZE] = [cx, cy];
-              }
-            }
-          }
-        }
         if (v === game.currentPlayer) {
-          input.set(x, y, 0, 1.0);
-          input.set(x, y, 0, 0.0);
+          input[i++] = 1.0;
+          input[i++] = 0.0;
         } else {
-          input.set(x, y, 0, 0.0);
-          input.set(x, y, 0, 1.0);
+          input[i++] = 0.0;
+          input[i++] = 1.0;
         }
       }
     }));
     if (game.prevPlay) {
-      const [cx, cy] = game.prevPlay;
-    } else {
-      candidates[Math.floor(BOARD_SIZE * BOARD_SIZE / 2)] = [
-        Math.floor(BOARD_SIZE / 2),
-        Math.floor(BOARD_SIZE / 2),
-      ];
+      const [x, y] = game.prevPlay;
+      input[(x + y * BOARD_SIZE) * 3] = 1.0;
     }
-    let sum = 0;
-    const choice = [];
-    Object.keys(candidates).forEach(k => {
-      k = k | 0;
-      let j = k * 2;
-      const pos = candidates[k];
-      input.set(pos[0], pos[1], 0, 1.0);
-      const value = this.net.forward(input).w[0] + 0.00001;
-      if (value === NaN) {
-        throw new Error('Evaluated NaN');
+    while (true) {
+      const decision = this.brain.act(input);
+      const x = decision % BOARD_SIZE;
+      const y = Math.floor(decision / BOARD_SIZE);
+      if (game.board[y][x] === EMPTY) {
+        this.willReward = 0;
+        this.shouldLearn = true;
+        callback(x, y);
+        break;
+      } else {
+        this.brain.learn(-100);
       }
-      choice.push({ pos, value });
-      sum += value;
-      input.set(pos[0], pos[1], 0, 0.0);
-    });
-    if (choice.length > 0) {
-      choice.sort((a, b) => b.value - a.value);
-      let decision = choice[0];
-      let watermark = 0;
-      choice.forEach(v => {
-        const propability = v.value / sum;
-        watermark += propability;
-        v.propability = propability;
-        v.watermark = watermark;
-      });
-      //const r = Math.random();
-      //for (let i = 0; i < choice.length; i++) {
-      //  if (r < choice[i].watermark) {
-      //    decision = choice[i];
-      //    break;
-      //  }
-      //}
-      const [x, y] = decision.pos;
-      callback(x, y);
-    } else {
-      throw new Error('No place left to play');
+    }
+  }
+
+  learnFromReward () {
+    if (this.shouldLearn) {
+      this.brain.learn(this.willReward);
+      this.shouldLearn = false;
     }
   }
 
   end () {
+    this.learnFromReward();
   }
 }
 
